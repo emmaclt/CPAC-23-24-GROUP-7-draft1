80e5e614c01bb377707e6933d593a24af3171b50
Enrico Dalla Mora <enrico.dalla@hotmail.it>
2024-04-27 18:36:14 +0200

    Midi processing update: -note duration -midi data mapping to different range FILES TO BE CONSIDERED: play_enrico my_chain realtime_midi_osc

--- my_chain.py
+++ my_chain.py
@@ -40,35 +40,3 @@ class VariableLengthMarkovChain:
                 return next_state
         raise ValueError("State not found in the Markov chain")
 
-
-
-
-
-
-'''
-#INITIALIZE
-dataset = [1, 2, 3, 4, 5, 3, 3, 2, 4, 1, 1, 2, 3, 5, 4, 2, 3, 3, 1, 5, 3, 2, 1, 1, 3, 5, 3, 5, 2, 3, 1, 5, 4, 3, 2, 1, 5, 5, 3, 3]
-#Max order for which the chain and probability matrix are computed (it will also be computed for all orders lower than the max one)
-max_order = 12 
-#Max order considered for generating new states (can be dynamically changed by the closeness parameter coming from touchdesigner via osc)
-set_order = 8  
-markov_chain = VariableLengthMarkovChain(max_order, dataset)
-#print(markov_chain.transitions)
-
-
-
-
-#GENERATE
-state = tuple(dataset[0:set_order])
-for i in range(100):
-    next_state = markov_chain.generate(state, set_order)
-    print("Next state after", state, ":", next_state)
-    #update current state with next state requires the tuple to be converted to list,
-    #manipulated and converted back to tuple
-    update_state=list(state)
-    update_state.pop(0)
-    update_state.append(next_state)
-    state=tuple(update_state)
-    #control generation/printing flow (currentlty every 1 second)
-    time.sleep(1)
-'''
--- play_enrico.py
+++ play_enrico.py
@@ -9,14 +9,24 @@ import re
 from mido import MidiFile, MidiTrack
 from mido import Message, MetaMessage
 
-max_order=5
-set_order=5
+#PARAMETERS------------------------------------------------------------------------------
+
+max_order=8
+set_order=1
+velocity_quantization=32
+time_quantization=64
+dur_quantization=64
+tempo_modifier=8
 dataset_directory=Path("./maestro-v3.0.0/test")
 
 
 
-#ENCODE METHOD
 
+
+
+#SETUP-----------------------------------------------------------------------------------
+
+#Function to encode midi files into strings
 def encode(file: mido.MidiFile) -> List[str]:
     notes = []
 
@@ -28,10 +38,7 @@ def encode(file: mido.MidiFile) -> List[str]:
 
     return notes
 
-
-
-# Function to set the tempo and the bpm of the dataset midi files 
-
+#Function to set the tempo and the bpm of the dataset midi files 
 def setup_time(file: mido.MidiFile):
     for message in file:
         if message.type == 'set_tempo':
@@ -41,12 +48,41 @@ def setup_time(file: mido.MidiFile):
 
     return tempo, bpm
 
+#Function to change map variables from one range to another
+def range_map(OldMax, OldMin, NewMax, NewMin, OldValue):
+    OldRange = (OldMax - OldMin)  
+    NewRange = (NewMax - NewMin)  
+    NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+    return NewValue
+
+#Function to menage the received OSC message for closeness (set order)
+def handle_message(address, tags, data, client_address):
+    global set_order
+    set_order = data[0]
+    print(f"Ricevuto messaggio da {address}: {set_order}")
+
+#Function to run the server in a separated thread
+def run_server():
+    server = pyOSC3.OSCServer(("127.0.0.1", 57000))
+    server.addMsgHandler("/closeness", handle_message)
+    print(f"In ascolto")
+    # Run the server
+    server.serve_forever()
 
 
-#GENERATE MODEL
+
+
+
+
+
+
+#MIDI PROCESSING-------------------------------------------------------------------------
 
 dataset_notes = []
+notes_int=[]
+notes_int_dur=[]
 
+#Read midi files, encode and store data in dataset_notes
 for filename in dataset_directory.iterdir():
     with mido.MidiFile(filename) as file:
         notes = encode(file)
@@ -54,12 +90,74 @@ for filename in dataset_directory.iterdir():
     dataset_notes.extend(notes)
     print(f"Processed {filename}")    
 
-
+#Print stuff for control purposes, to be deleated once the code is definitive
 with open("Input.txt", "w") as text_file:
     print(f"Note: "+str(dataset_notes), file=text_file)
 
-print(len(dataset_notes))
 
+#Convert midi infos from strings to numbers [dataset_notes->notes_int]
+for i in range(len(dataset_notes)):
+    temp = re.findall(r'\d+', dataset_notes[i])
+    res = list(map(int, temp))
+    notes_int.append([res[1], res[2], res[3]])
+
+#Print stuff for control purposes, to be deleated once the code is definitive
+with open("Notes_int.txt", "w") as text_file:
+    print(f""+str(notes_int), file=text_file)
+
+#Midi information processing: [notes_int->notes_int_dur]
+#Extracts the duration (dur) feature by evaluating after how much time the note 
+#velocity is set to 0
+#Removes 0 velocity messages and adds a 4th parameter: dur
+for i in range(len(notes_int)-1):
+    if notes_int[i][1]!=0:
+        temp=0
+        for j in range(i+1, len(notes_int)):
+            temp+=notes_int[j][2]
+            if notes_int[i][0]==notes_int[j][0] and notes_int[j][1]==0:
+                notes_int[i].append(temp)
+                notes_int_dur.append(notes_int[i])
+                #adds the time interval from the 0 velocity removed message 
+                #to previous one in order to keep time consistency
+                notes_int[j-1][2]+=notes_int[j][2]
+                break
+            
+#Print stuff for control purposes, to be deleated once the code is definitive
+with open("Notes_int_dur.txt", "w") as text_file:
+    print(f""+str(notes_int_dur), file=text_file)
+
+#Change the range of values of notes_int_dur for velocity time and dur in order to
+#higher the chance of one tuple appearing more than once 
+#(i.e. making the generation less deterministic)
+velocity_values = [row[1] for row in notes_int_dur]
+max_velocity_value = max(velocity_values)
+for row in notes_int_dur:
+    row[1]=round(range_map(max_velocity_value, 0, velocity_quantization, 0, row[1]))
+
+time_values = [row[2] for row in notes_int_dur]
+max_time_value = max(time_values)
+for row in notes_int_dur:
+    row[2]=round(range_map(max_time_value, 0, time_quantization, 0, row[2]))
+
+dur_values = [row[3] for row in notes_int_dur]
+max_dur_value = max(dur_values)
+for row in notes_int_dur:
+    row[3]=round(range_map(max_dur_value, 0, dur_quantization, 0, row[3]))
+
+#Print stuff for control purposes, to be deleated once the code is definitive
+with open("Notes_int_dur_ranged.txt", "w") as text_file:
+    print(f""+str(notes_int_dur), file=text_file)
+
+#Convert back to tuple as to be consistent with the following sections of code
+#Overwriting dataset_notes [notes_int_dur->a->dataset_notes]
+a=[]
+for i in range(len(notes_int_dur)):
+    a.append(""+str(notes_int_dur[i][0])+" "+str(notes_int_dur[i][1])+" "+str(notes_int_dur[i][2])+" "+str(notes_int_dur[i][3]))
+dataset_notes=a
+
+#Print stuff for control purposes, to be deleated once the code is definitive
+with open("dataset_notes.txt", "w") as text_file:
+    print(f""+str(a), file=text_file)
 
 #set tempo, bpm and ticks per beat
 first_file_midi = next(dataset_directory.iterdir())
@@ -68,142 +166,81 @@ with mido.MidiFile(first_file_midi) as file:
     ticks_per_beat = file.ticks_per_beat
 
 print(tempo, bpm, ticks_per_beat)
+tempo*=tempo_modifier
 
 
 
-# Function to menage the received OSC messages
-def handle_message(address, tags, data, client_address):
-    global set_order
-    set_order = data[0]
-    print(f"Ricevuto messaggio da {address}: {set_order}")
 
 
-# Function to run the server in a separated thread
-def run_server():
-    
-    server = pyOSC3.OSCServer(("127.0.0.1", 57000))
-    server.addMsgHandler("/closeness", handle_message)
-
-    print(f"In ascolto")
-    
-    # Run the server
-    server.serve_forever()
-
-server_thread = Thread(target=run_server)
-server_thread.start()
 
 
+#CHAIN GENERATION----------------------------------------------------------------------
+
+#Chain model and matrix generation
 markov_chain_notes = VariableLengthMarkovChain(max_order, dataset_notes)
 
 
 
 
 
-#GENERATE NOTES
 
+#NOTES GENERATION----------------------------------------------------------------------
+
+#Receive closeness (set_order) data
+server_thread = Thread(target=run_server)
+server_thread.start()
+
+#Run client to send generated notes
 client = pyOSC3.OSCClient()
 client.connect( ( '127.0.0.1', 57120 ) )
+
+#Initialize generation starting tuple from one of the existing files in the dataset
 state_notes = tuple(dataset_notes[0:set_order])
+
 #list containing all the generated states from which (line 121) 
 #the current state is extracted based on the set_order
 all_states=[] 
 all_states.extend(list(state_notes))
-text=[]
 
 #initialize output file to blank (file per controllare se le stringhe generate hanno senso)
 with open("Output.txt", "w") as text_file:
         print(f"", file=text_file)
 
-i=0
-
-while i<1000:
-    print(set_order)
+#Generation
+while True:
     state_notes=tuple(all_states[(len(all_states)-set_order):len(all_states)])
     next_state_notes = markov_chain_notes.generate(state_notes, set_order)
     all_states.append(next_state_notes)
 
-    #effettiva scrittura del file di controllo 
+    #Print stuff for control purposes, to be deleated once the code is definitive 
     with open("Output.txt", "a") as text_file:
         print(f"Note: "+str(next_state_notes), file=text_file)
-
-    text.append(str(next_state_notes))
-    #print("Note: "+str(next_state_notes))
     
+    #Print stuff for control purposes, to be deleated once the code is definitive
     with open("state_notes.txt", "a") as text_file:
         print(f"Note: "+str(state_notes), file=text_file)
 
     #divide string and extract numbers to be sent via OSC
+    #values are re-ranged to their original values
     temp = re.findall(r'\d+', next_state_notes)
     res = list(map(int, temp))
-    next_state_notes=res[1]
-    next_state_velocity=res[2]
-    next_state_time=res[3]
+    next_state_notes=res[0]
+    next_state_velocity=range_map(velocity_quantization, 0, max_velocity_value, 0, res[1])
+    next_state_time=range_map(time_quantization, 0, max_time_value, 0, res[2])
+    next_state_dur= mido.tick2second(range_map(dur_quantization, 0, max_dur_value, 0, res[3]), ticks_per_beat, tempo)
 
     #prepare the message
     msg = pyOSC3.OSCMessage()
     msg.setAddress("/numbers")
-    out=[next_state_notes, next_state_velocity, next_state_time]
+    out=[next_state_notes, next_state_velocity, next_state_time, next_state_dur]
     msg.append(out)
     
     #compute the delta time, it's the time that should pass between the last note played and the current note
-    delta_time = mido.tick2second(next_state_time, ticks_per_beat, tempo*2)
+    delta_time = mido.tick2second(next_state_time, ticks_per_beat, tempo)
     print(delta_time)
     time.sleep(delta_time)
 
     #print and send
     print(res)
+    print("")
     client.send(msg)
-
-
-    i=i+1
-    
-    
-
-
-
-
-
-
-
-
-
-def decode_meta_message(line: str) -> MetaMessage:
-    args = line[12:-1].split(", ")
-    message_type = args.pop(0)[1:-1]
-
-    #args = [argument.split("=") for argument in args]
-    kwargs = {}
-
-    for argument in args:
-        key, value = argument.split("=")
-        
-        if value.startswith("'") and value.endswith("'"): # string, we gotta remove the quotes
-            value = value[1:-1]
-        elif value.isnumeric():
-            value = int(value)
-
-        kwargs[key] = value
-    
-    #print(f"Decoded meta message: {message_type} {kwargs}")
-    return MetaMessage(message_type, **kwargs)
-
-def decode(text: List[str]) -> MidiFile:
-    track = MidiTrack()
-
-    for line in text:
-        if line.startswith("MetaMessage"):
-            track.append(decode_meta_message(line))
-            continue
-
-        try:
-            track.append(Message.from_str(line))
-        except:
-            print(f"Failed to decode message: {line}")
-    
-    return MidiFile(tracks=[track])
-
-
-MIDI_output=Path("./song.mid")
-midi = decode(text)
-midi.save(MIDI_output)
-
